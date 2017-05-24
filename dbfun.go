ab3b862965f7a1a8100976c718d6ddde8b3881e0
linlexing <linlexing@gmail.com>
2017-05-24 10:02:38 +0800

    改正rollback后err变为nil的错误

--- dbfun.go
+++ dbfun.go
@@ -781,7 +781,7 @@ func RunAtTx(db *sqlx.DB, callback func(DB) error) (err error) {
 		}
 	}()
 	if err = callback(tx); err != nil {
-		err = tx.Rollback()
+		tx.Rollback()
 	} else {
 		err = tx.Commit()
 	}
