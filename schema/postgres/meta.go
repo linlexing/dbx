eddd88cf4d591c41b6f6c63e0bc070f3f73f692d
GuanZhenzhen <g18751528841@gmail.com>
2017-06-01 17:11:01 +0800

    判断表是否存在的sql语法问题

--- schema/postgres/meta.go
+++ schema/postgres/meta.go
@@ -89,8 +89,6 @@ func (m *meta) TableExists(db common.DB, tabName string) (bool, error) {
 	return iCount > 0, nil
 }
 
-
-
 func (m *meta) CreateTable(db common.DB, tab *schema.Table) error {
 	cols := []string{}
 	for _, v := range tab.Columns {
