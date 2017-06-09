71c7c1684853daf14a8dfe32aa788b1fd2dfa1c5
linlexing <linlexing@gmail.com>
2017-06-09 16:21:49 +0800

    fix：字段定义修改的判断

--- schema/column.go
+++ schema/column.go
@@ -23,6 +23,8 @@ func (f *Column) Eque(src *Column) bool {
 		len(f.TrueType) > 0 && len(src.TrueType) > 0 {
 		return f.TrueType == src.TrueType
 	}
+	//历史原因，MaxLength <=0 只有一个含义，无限的长度
+	//历史代码中有时用了-1,有时是0，所以都是<=0的视为相等
 	return f.Type == src.Type &&
 		(f.MaxLength == src.MaxLength ||
 			f.MaxLength <= 0 && src.MaxLength <= 0) &&
