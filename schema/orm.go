f618da4304dbbf48f37ce219b29ba87039c74b07
linlexing <linlexing@gmail.com>
2017-06-11 22:15:39 +0800

    初步完成orm，已经其测试用例

--- schema/orm.go
+++ schema/orm.go
@@ -4,66 +4,265 @@ import (
 	"errors"
 	"reflect"
 
+	"bytes"
+	"encoding/gob"
+	"encoding/json"
+	"fmt"
 	"strings"
 	"time"
 )
 
 type structField struct {
-	name    string  //结构属性名称
-	define  *colDef //字段定义
-	st      reflect.Type
-	valPath []int //属性路径，即每层属性序号
+	fieldName string       //结构属性名称
+	define    *colDef      //字段定义
+	st        reflect.Type //Go类型
+	child     bool         //是否明细表
+	childName string       //如是明细表，这里是表名称
+	valPath   []int        //属性路径，即每层属性序号
 }
 
-func (s *structField) get(obj reflect.Value) interface{} {
-	p := obj
-	for _, i := range s.valPath {
-		p = p.Field(i)
+//checkType 检查数据库类型和实际类型是否相容
+//数据库类型 Go类型
+//String string struct slice map
+//Bytea  []byte struct slice map
+//Datetime time.Time
+//Float  float64
+//Int    int64
+//child []struct
+func (s *structField) checkType() error {
+	dt := s.define.Type
+	st := s.st
+	if s.child {
+		if st.Kind() == reflect.Slice &&
+			st.Elem().Kind() == reflect.Struct {
+			return nil
+		}
+		return errors.New("child table must is slice of struct")
 	}
+	switch dt {
+	case TypeString:
+		switch st.Kind() {
+		case
+			reflect.String,
+			reflect.Struct,
+			reflect.Slice,
+			reflect.Map:
+			return nil
+		}
+		return fmt.Errorf("string type must be one of string、Struct、Slice、Map")
+	case TypeBytea:
+		//[]byte 已经在下面的slice的范围中
+		//if st.Kind() == reflect.Slice && st.Elem().Kind() == reflect.Uint8 {
+		//	return nil
+		//}
+		switch st.Kind() {
+		case
+			reflect.Struct,
+			reflect.Slice,
+			reflect.Map:
+			return nil
+		}
+		return fmt.Errorf("typea type must be one of []byte、Struct、Slice、Map")
+	case TypeDatetime:
+		if st.ConvertibleTo(reflect.TypeOf(time.Time{})) {
+			return nil
+		}
+		return fmt.Errorf("datetime type must is time.Time")
+	case TypeFloat:
+		if st.Kind() == reflect.Float64 {
+			return nil
+		}
+		return fmt.Errorf("float type must is float64")
+	case TypeInt:
+		if st.Kind() == reflect.Int64 {
+			return nil
+		}
+		return fmt.Errorf("int type must is int64")
+	default:
+		return errors.New("invalid type:" + dt.String())
+	}
+}
 
-	//空字符串返回nil
+//json 指示该字段是否用json转换
+func (s *structField) json() bool {
+	return !s.child && s.define.Type == TypeString &&
+		(s.st.Kind() == reflect.Struct ||
+			s.st.Kind() == reflect.Slice ||
+			s.st.Kind() == reflect.Map)
+}
+
+//gob 指示该字段是否用gob转换
+func (s *structField) gob() bool {
+	if s.child {
+		return false
+	}
+	if s.st.Kind() == reflect.Slice &&
+		s.st.Elem().Kind() == reflect.Uint8 {
+		return false
+	}
+	return s.define.Type == TypeBytea &&
+		(s.st.Kind() == reflect.Struct ||
+			s.st.Kind() == reflect.Slice ||
+			s.st.Kind() == reflect.Map)
+}
+
+//isZero 判断一个值是否是0值
+func (s *structField) isZero(val reflect.Value) bool {
 	switch s.st.Kind() {
-	case reflect.String:
-		if s.define.Null && len(p.String()) == 0 {
-			return nil
+	case reflect.Slice, reflect.Map:
+		if val.IsNil() {
+			return true
 		}
 	}
-	//0时间返回nil
-	switch tv := p.Interface().(type) {
-	case time.Time:
-		if s.define.Null && tv.IsZero() {
-			return nil
+	if s.child {
+		return val.Len() == 0
+	}
+	switch s.define.Type {
+	case TypeString:
+		switch s.st.Kind() {
+		case reflect.String, reflect.Slice, reflect.Map:
+			return val.Len() == 0
+		case reflect.Struct:
+			return reflect.DeepEqual(reflect.Zero(s.st), val.Interface())
 		}
+		panic("invalid type")
+	case TypeBytea:
+		switch s.st.Kind() {
+		case reflect.Slice, reflect.Map:
+			return val.Len() == 0
+		case reflect.Struct:
+			return reflect.DeepEqual(reflect.Zero(s.st), val)
+		}
+		panic("invalid type")
+	case TypeDatetime:
+		return val.Interface().(time.Time).IsZero()
+	case TypeFloat:
+		return val.Interface().(float64) == 0
+	case TypeInt:
+		return val.Interface().(int64) == 0
+	default:
+		panic("invalid dbtype")
+	}
+}
+
+//get 会自动转换
+//空值且数据库可为空返回nil
+//str 和 bytea类型字段，如果值是struct slice map，则返回json 和 gob
+func (s *structField) get(obj reflect.Value) (interface{}, error) {
+	p := s.getv(obj)
+	//子表必定是[]struct
+	if s.child {
+		if s.isZero(p) {
+			return nil, nil
+		}
+		clist := []map[string]interface{}{}
+		for i := 0; i < p.Len(); i++ {
+			cm, cd, e := struct2Row(p.Index(i))
+			if e != nil {
+				return nil, e
+			}
+			//子表不能再有子表，防止问题复杂化
+			if len(cd) > 0 {
+				err := fmt.Errorf("field:%s has sub child data", s.fieldName)
+				return nil, err
+			}
+			clist = append(clist, cm)
+		}
+		return clist, nil
+	}
+	if s.define.Null && s.isZero(p) {
+		return nil, nil
+	}
+	if s.json() {
+
+		bys, err := json.Marshal(p.Interface())
+		if err != nil {
+			return nil, err
+		}
+		return string(bys), nil
+	}
+	if s.gob() {
+
+		bys := bytes.NewBuffer(nil)
+		if err := gob.NewEncoder(bys).EncodeValue(p); err != nil {
+			return nil, err
+		}
+		return bys.Bytes(), nil
 	}
 
-	return p.Interface()
+	return p.Interface(), nil
 }
-func (s *structField) set(obj reflect.Value, val interface{}) {
+func (s *structField) getv(obj reflect.Value) reflect.Value {
 	p := obj
 	for _, i := range s.valPath {
 		p = p.Field(i)
 	}
-	//nil特殊处理
-	if val == nil {
-		//字符串设置成空串
-		switch s.st.Kind() {
-		case reflect.String:
-			p.SetString("")
-			return
-		}
-		//时间设置成0值
-		switch p.Interface().(type) {
-		case time.Time:
-			p.Set(reflect.ValueOf(time.Time{}))
-			return
-		}
+	for p.Kind() == reflect.Ptr {
+		p = p.Elem()
 	}
-	p.Set(reflect.ValueOf(val))
+	return p
+}
+func (s *structField) set(obj reflect.Value, val interface{}) error {
+	//nil 设置为0值
+	if val == nil {
+		s.setv(obj, reflect.Zero(s.st))
+		return nil
+	}
+	//子表
+	if s.child {
+		list := reflect.New(s.st)
+		for _, row := range val.([]map[string]interface{}) {
+			rv := reflect.New(s.st.Elem())
+			if err := row2Struct(row, nil, rv); err != nil {
+				return err
+			}
+			list.Elem().Set(reflect.Append(list.Elem(), rv.Elem()))
+		}
+		s.setv(obj, list.Elem())
+		return nil
+	}
+	if s.json() {
+		newv := reflect.New(s.st)
+		if err := json.Unmarshal([]byte(val.(string)),
+			newv.Interface()); err != nil {
+			return err
+		}
+		s.setv(obj, newv.Elem())
+		return nil
+	}
+	if s.gob() {
+		newv := reflect.New(s.st)
+		if err := gob.NewDecoder(bytes.NewBuffer(val.([]byte))).
+			Decode(newv.Interface()); err != nil {
+			return err
+		}
+		s.setv(obj, newv.Elem())
+		return nil
+	}
+	s.setv(obj, reflect.ValueOf(val))
+	return nil
+}
+func (s *structField) setv(obj reflect.Value, val reflect.Value) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(s.fieldName, s.st)
+			panic(r)
+		}
+	}()
+	p := obj
+	for _, i := range s.valPath {
+		p = p.Field(i)
+	}
+	p.Set(val)
 }
 
+//fieldsFromStruct 读取一个结构体，转换成元数据，可以接受一个*struct、
+//[]struct、[]*struct,并需传入一个属性路径索引数组，方便后期赋值
+//允许匿名嵌套
 func fieldsFromStruct(vtype reflect.Type, parentPath []int) (rev []*structField, err error) {
 	rev = []*structField{}
-	if vtype.Kind() == reflect.Ptr {
+	for vtype.Kind() == reflect.Slice ||
+		vtype.Kind() == reflect.Ptr {
 		vtype = vtype.Elem()
 	}
 	if vtype.Kind() != reflect.Struct {
@@ -72,9 +271,10 @@ func fieldsFromStruct(vtype reflect.Type, parentPath []int) (rev []*structField,
 	}
 	for i := 0; i < vtype.NumField(); i++ {
 		field := vtype.Field(i)
-		newPath := []int{}
+		newPath :=make([]int,len(parentPath))
 		copy(newPath, parentPath)
 		newPath = append(newPath, i)
+		//嵌套结构，扁平化
 		if field.Anonymous {
 			list, e := fieldsFromStruct(field.Type, newPath)
 			if e != nil {
@@ -85,9 +285,9 @@ func fieldsFromStruct(vtype reflect.Type, parentPath []int) (rev []*structField,
 			continue
 		}
 		sf := &structField{
-			name:    field.Name,
-			valPath: newPath,
-			st:      field.Type,
+			fieldName: field.Name,
+			valPath:   newPath,
+			st:        field.Type,
 		}
 		name := strings.ToUpper(field.Name)
 		tag, ok := field.Tag.Lookup("dbx")
@@ -104,89 +304,141 @@ func fieldsFromStruct(vtype reflect.Type, parentPath []int) (rev []*structField,
 		//如果有定义，则必定是类型名称或者字段名+类型
 		tags := strings.Fields(tag)
 
-		//如果不是任意一种类型名，则说明是完整的定义，即名称开始
+		//如果不是任意一种类型名或子表，则说明是完整的定义，即名称开始
 		if !strings.HasPrefix(tags[0], TypeString.String()) &&
 			!strings.HasPrefix(tags[0], TypeBytea.String()) &&
 			!strings.HasPrefix(tags[0], TypeDatetime.String()) &&
 			!strings.HasPrefix(tags[0], TypeFloat.String()) &&
-			!strings.HasPrefix(tags[0], TypeInt.String()) {
-
-			sf.define, err = columnDefine(tag)
-			if err != nil {
-				return
+			!strings.HasPrefix(tags[0], TypeInt.String()) &&
+			!strings.HasPrefix(tags[0], "CHILD") {
+			//只有两列，末列是CHILD的，说明是子表
+			if len(tags) == 2 && tags[1] == "CHILD" {
+				sf.child = true
+				sf.childName = tags[0]
+			} else {
+				sf.define, err = columnDefine(tag)
+				if err != nil {
+					return
+				}
 			}
 			rev = append(rev, sf)
 			continue
 		}
 		//到这就说明，是省略名称的定义，补上大写的名称
-		sf.define, err = columnDefine(name + " " + tag)
-		if err != nil {
-			return
+		if tag == "CHILD" {
+			sf.child = true
+			sf.childName = name
+		} else {
+			sf.define, err = columnDefine(name + " " + tag)
+			if err != nil {
+				return
+			}
+		}
+		//检查字段类型是否正确
+		if err := sf.checkType(); err != nil {
+			return nil, fmt.Errorf("field:%s error:%s", sf.fieldName, err)
 		}
 		rev = append(rev, sf)
 	}
 	return rev, nil
 }
-
-//TableFromStruct 将一个struct转换成table
-func TableFromStruct(tableName string, meta interface{}) (*Table, error) {
-	vtype := reflect.TypeOf(meta)
-	if vtype.Kind() == reflect.Ptr {
-		vtype = vtype.Elem()
-	}
-	list, err := fieldsFromStruct(vtype, nil)
+func struct2Table(tableName string, vtype reflect.Type, parentPath []int) ([]*Table, error) {
+	list, err := fieldsFromStruct(vtype, parentPath)
 	if err != nil {
 		return nil, err
 	}
 	//剥离出其中的定义
+	result := []*Table{NewTable(tableName)}
 	coldefs := []*colDef{}
 	for _, one := range list {
-		coldefs = append(coldefs, one.define)
+
+		//子表递归调用
+		if one.child {
+			tabs, err := struct2Table(one.childName, one.st, parentPath)
+			if err != nil {
+				return nil, err
+			}
+			result = append(result, tabs...)
+		} else {
+			coldefs = append(coldefs, one.define)
+		}
 	}
-	tab := NewTable(tableName)
-	tab.Columns, tab.PrimaryKeys = columnsDefine(coldefs)
-	return tab, nil
+
+	result[0].Columns, result[0].PrimaryKeys = columnsDefine(coldefs)
+	return result, nil
+
+}
+
+//TableFromStruct 将一个struct转换成table清单,可能有明细表
+//第一个是主表，剩余是明细表
+func TableFromStruct(tableName string, meta interface{}) ([]*Table, error) {
+	return struct2Table(tableName, reflect.TypeOf(meta), nil)
+}
+func struct2Row(vval reflect.Value) (main map[string]interface{},
+	child map[string][]map[string]interface{}, err error) {
+	types, err := fieldsFromStruct(vval.Type(), nil)
+	if err != nil {
+		return
+	}
+	if vval.Kind() == reflect.Ptr {
+		vval = vval.Elem()
+	}
+	main = map[string]interface{}{}
+	child = map[string][]map[string]interface{}{}
+	for _, v := range types {
+		var clist interface{}
+		if clist, err = v.get(vval); err != nil {
+			return
+		}
+		if v.child {
+			if clist != nil {
+				child[v.childName] = clist.([]map[string]interface{})
+			}
+		} else {
+			if main[v.define.Name], err = v.get(vval); err != nil {
+				return
+			}
+		}
+	}
+	return
 }
 
 //Struct2Row 结构体的值转换成map
-func Struct2Row(meta interface{}) (map[string]interface{}, error) {
-	vtype := reflect.TypeOf(meta)
-	if vtype.Kind() == reflect.Ptr {
-		vtype = vtype.Elem()
-	}
-	types, err := fieldsFromStruct(vtype, nil)
-	if err != nil {
-		return nil, err
-	}
-	vval := reflect.ValueOf(meta)
-	if vval.Kind() == reflect.Ptr {
-		vval = vval.Elem()
-	}
-	result := map[string]interface{}{}
-	for _, v := range types {
-		result[v.define.Name] = v.get(vval)
-	}
-	return result, nil
+func Struct2Row(meta interface{}) (main map[string]interface{},
+	detail map[string][]map[string]interface{}, err error) {
+	return struct2Row(reflect.ValueOf(meta))
 }
 
 //Row2Struct map转换成结构体的值
-func Row2Struct(row map[string]interface{}, meta interface{}) error {
-	vtype := reflect.TypeOf(meta)
-	if vtype.Kind() != reflect.Ptr {
-		return errors.New("the meta must is ptr")
-	}
-	vtype = vtype.Elem()
-	types, err := fieldsFromStruct(vtype, nil)
+func Row2Struct(row map[string]interface{},
+	child map[string][]map[string]interface{}, vval interface{}) error {
+	return row2Struct(row, child, reflect.ValueOf(vval))
+}
+func row2Struct(row map[string]interface{},
+	child map[string][]map[string]interface{}, vval reflect.Value) error {
+	types, err := fieldsFromStruct(vval.Type(), nil)
 	if err != nil {
 		return err
 	}
-	vval := reflect.ValueOf(meta)
-	if vval.Kind() == reflect.Ptr {
+	for vval.Kind() == reflect.Ptr {
 		vval = vval.Elem()
 	}
+	/*	for vval.Kind() != reflect.Ptr {
+		return fmt.Errorf("vval must is ptr")
+	}*/
 	for _, v := range types {
-		if tv, ok := row[v.define.Name]; ok {
-			v.set(vval, tv)
+		if v.child {
+			if clist, ok := child[v.childName]; ok {
+				v.set(vval, clist)
+			} else {
+				v.set(vval, nil)
+			}
+		} else {
+			if tv, ok := row[v.define.Name]; ok {
+				v.set(vval, tv)
+			} else {
+				v.set(vval, nil) //没找到的属性要设置成nil，防止有旧值
+			}
 		}
 	}
 	return nil
--- schema/orm_test.go
+++ schema/orm_test.go
@@ -1,31 +1,63 @@
 package schema
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"reflect"
 	"testing"
 	"time"
 )
 
+type otherType struct {
+	Code string  `dbx:"STR(50) PRIMARY KEY"`
+	Name string  `dbx:"STR(50) PRIMARY KEY"`
+	Num  float64 `dbx:"FLOAT"`
+}
+type innerT struct {
+	C嵌入 string `dbx:"嵌入 STR(50)"`
+}
 type T用户 struct {
-	C代码 string  `dbx:"代码 STR(50) PRIMARY KEY"`
-	C名称 string  `dbx:"名称 STR(50) PRIMARY KEY"`
-	C描述 string  `dbx:"描述 STR"`
-	C数量 float64 `dbx:"数量 FLOAT NOT NULL INDEX"`
+	C代码  string       `dbx:"代码 STR(50) PRIMARY KEY"`
+	C名称  string       `dbx:"名称 STR(50) PRIMARY KEY"`
+	C描述  string       `dbx:"描述 STR"`
+	C时间  time.Time    `dbx:"时间 DATE"`
+	C数量  float64      `dbx:"数量 FLOAT NOT NULL INDEX"`
+	C其他  []*otherType `dbx:"其他 STR"`
+	C其他1 []otherType  `dbx:"其他1 BYTEA"`
+	C明细  []otherType  `dbx:"明细 CHILD"`
+	innerT
 }
 
+func init() {
+	gob.Register(T用户{})
+}
+func TestInnerStruct(t *testing.T) {
+	type tabT struct {
+		C代码 string `dbx:"代码 STR(50) PRIMARY KEY"`
+		C名称 string `dbx:"名称 STR(50) PRIMARY KEY"`
+		innerT
+	}
+	fields, err := fieldsFromStruct(reflect.TypeOf(tabT{}), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(fields[2].valPath, []int{2, 0}) {
+		t.Fatal("error", fields[2])
+	}
+}
 func TestTableFromStruct(t *testing.T) {
 	tt := []struct {
 		name string
-		tab1 *Table
-		tab2 *Table
+		tab1 []*Table
+		tab2 []*Table
 	}{struct {
 		name string
-		tab1 *Table
-		tab2 *Table
+		tab1 []*Table
+		tab2 []*Table
 	}{
 		"数据类型测试",
-		func() *Table {
+		func() []*Table {
 			tab := NewTable("TEST")
 			if err := tab.DefineScript(`
 			CODE  STR(50) PRIMARY KEY
@@ -40,92 +72,194 @@ func TestTableFromStruct(t *testing.T) {
 		`); err != nil {
 				t.Error(err)
 			}
-			return tab
-		}(), func() *Table {
+			return []*Table{tab}
+		}(), func() []*Table {
 			type test struct {
 				Code  string  `dbx:"STR(50) PRIMARY KEY"`
 				Name  string  `dbx:"STR(50) PRIMARY KEY"`
 				Desc  string  `dbx:"STR"`
 				Num   float64 `dbx:"FLOAT NOT NULL INDEX"`
-				Num1  int     `dbx:"INT"`
-				Num2  int
+				Num1  int64   `dbx:"INT"`
+				Num2  int64
 				Date1 time.Time `dbx:"DATE NULL INDEX"`
 				Date2 time.Time
 				Byte1 []byte `dbx:"bytea"`
 			}
-			tab, err := TableFromStruct("TEST", new(test))
+			tabs, err := TableFromStruct("TEST", new(test))
 			if err != nil {
 				t.Error(err)
 			}
-			return tab
+			return tabs
 		}()},
 		struct {
 			name string
-			tab1 *Table
-			tab2 *Table
+			tab1 []*Table
+			tab2 []*Table
 		}{
 			"汉字",
-			func() *Table {
+			func() []*Table {
 				tab := NewTable("用户")
 				if err := tab.DefineScript(`
-			代码  STR(50) PRIMARY KEY
-			名称  STR(50) PRIMARY KEY
-			描述  STR
-			数量  FLOAT NOT NULL INDEX
-		`); err != nil {
+					代码  STR(50) PRIMARY KEY
+					名称  STR(50) PRIMARY KEY
+					描述  STR
+					时间 DATE
+					数量  FLOAT NOT NULL INDEX
+					其他  STR
+					其他1 BYTEA
+					嵌入 STR(50)
+				`); err != nil {
 					t.Error(err)
 				}
-				return tab
-			}(), func() *Table {
+				tab1 := NewTable("明细")
+				if err := tab1.DefineScript(`
+					CODE  STR(50) PRIMARY KEY
+					name  STR(50) PRIMARY KEY
+					num  FLOAT
+				`); err != nil {
+					t.Error(err)
+				}
+				return []*Table{tab, tab1}
+			}(), func() []*Table {
 
-				tab, err := TableFromStruct("用户", new(T用户))
+				tabs, err := TableFromStruct("用户", new(T用户))
 				if err != nil {
 					t.Error(err)
 				}
-				return tab
+				return tabs
 			}()},
 	}
 	for _, v := range tt {
-		if err := tableEqu(v.tab1, v.tab2); err != nil {
-			t.Error(v.name, err)
+		if len(v.tab1) != len(v.tab2) {
+			t.Error(fmt.Errorf("%s len %d <> %d", v.name,
+				len(v.tab1), len(v.tab2)))
+		}
+		for i := range v.tab1 {
+			if err := tableEqu(v.tab1[i], v.tab2[i]); err != nil {
+				t.Error(fmt.Errorf("%s,tab:%s <> %s,%s", v.name,
+					v.tab1[i].Name, v.tab2[i].Name, err))
+			}
+
 		}
 	}
 }
+
 func TestStruct2Row(t *testing.T) {
-	tt := []struct {
-		name string
-		row1 map[string]interface{}
-		row2 map[string]interface{}
-	}{struct {
-		name string
-		row1 map[string]interface{}
-		row2 map[string]interface{}
-	}{
-		"基本测试",
-		func() map[string]interface{} {
-			user := &T用户{
-				C代码: "001",
-				C名称: "name",
-				C数量: 11.2,
-			}
-			row, err := Struct2Row(user)
-			if err != nil {
-				t.Error(err)
-			}
-			return row
-		}(),
-		map[string]interface{}{
-			"代码": "001",
-			"名称": "name",
-			"描述": nil,
-			"数量": float64(11.2),
+	var row1 map[string]interface{}
+	var child1 map[string][]map[string]interface{}
+	var row2 map[string]interface{}
+	var child2 map[string][]map[string]interface{}
+	user := &T用户{
+		C代码: "001",
+		C名称: "name",
+		C数量: 11.2,
+		C描述: "",
+		C时间: time.Time{},
+		C其他: []*otherType{
+			&otherType{
+				Name: "n1",
+				Code: "c1",
+				Num:  111.22,
+			}, &otherType{
+				Name: "n2",
+				Code: "c2",
+				Num:  111.11,
+			},
 		},
-	},
+		C其他1: []otherType{
+			otherType{
+				Name: "n1",
+				Code: "c1",
+				Num:  111.22,
+			}, otherType{
+				Name: "n2",
+				Code: "c2",
+				Num:  111.11,
+			},
+		},
+		C明细: []otherType{
+			otherType{
+				Name: "n1",
+				Code: "c1",
+				Num:  111.22,
+			}, otherType{
+				Name: "n2",
+				Code: "c2",
+				Num:  111.11,
+			},
+		},
 	}
-	for _, one := range tt {
-		if err := mapEqu(one.row1, one.row2); err != nil {
-			t.Error(err)
+
+	row1, child1, err := Struct2Row(user)
+	if err != nil {
+		t.Error(err)
+	}
+	bys := bytes.NewBuffer(nil)
+	if err := gob.NewEncoder(bys).Encode([]otherType{
+		otherType{
+			Name: "n1",
+			Code: "c1",
+			Num:  111.22,
+		}, otherType{
+			Name: "n2",
+			Code: "c2",
+			Num:  111.11,
+		},
+	}); err != nil {
+		t.Error(err)
+	}
+	row2 = map[string]interface{}{
+		"代码":  "001",
+		"名称":  "name",
+		"描述":  nil,
+		"时间":  nil,
+		"数量":  float64(11.2),
+		"其他":  `[{"Code":"c1","Name":"n1","Num":111.22},{"Code":"c2","Name":"n2","Num":111.11}]`,
+		"其他1": bys.Bytes(),
+		"嵌入":  nil,
+	}
+	child2 = map[string][]map[string]interface{}{
+		"明细": []map[string]interface{}{
+			map[string]interface{}{
+				"CODE": "c1",
+				"NAME": "n1",
+				"NUM":  float64(111.22),
+			},
+			map[string]interface{}{
+				"CODE": "c2",
+				"NAME": "n2",
+				"NUM":  float64(111.11),
+			},
+		},
+	}
+	if err := mapEqu(row1, row2); err != nil {
+		t.Error(err)
+	}
+	if len(child1) != len(child2) {
+		t.Error("child not equ")
+	}
+	for k := range child1 {
+		c1 := child1[k]
+		c2 := child2[k]
+		if len(c1) != len(c2) {
+			t.Error("child ", k, " rows not equ")
 		}
+		for i := range c1 {
+			if err := mapEqu(c1[i], c2[i]); err != nil {
+				t.Error("child", k, "row", i, err)
+			}
+		}
+	}
+	//测试时间
+	dt := time.Date(2016, 1, 1, 12, 1, 1, 0, time.Local)
+	user.C时间 = dt
+	row1, child1, err = Struct2Row(user)
+	if err != nil {
+		t.Error(err)
+	}
+	row2["时间"] = time.Date(2016, 1, 1, 12, 1, 1, 0, time.Local)
+	if err := mapEqu(row1, row2); err != nil {
+		t.Error(err)
 	}
 }
 
@@ -141,13 +275,43 @@ func TestRow2Struct(t *testing.T) {
 	}{
 		"基本测试",
 		func() *T用户 {
+			bys := bytes.NewBuffer(nil)
+			if err := gob.NewEncoder(bys).Encode([]otherType{
+				otherType{
+					Name: "n1",
+					Code: "c1",
+					Num:  111.22,
+				}, otherType{
+					Name: "n2",
+					Code: "c2",
+					Num:  111.11,
+				},
+			}); err != nil {
+				t.Error(err)
+			}
 			user := &T用户{}
 			if err := Row2Struct(
 				map[string]interface{}{
-					"代码": "001",
-					"名称": "name",
-					"描述": nil,
-					"数量": float64(11.2),
+					"代码":  "001",
+					"名称":  "name",
+					"描述":  nil,
+					"数量":  float64(11.2),
+					"其他":  `[{"name":"n1","code":"c1","num":111.22},{"name":"n2","code":"c2","num":111.11}]`,
+					"其他1": bys.Bytes(),
+					"嵌入":  "aaa",
+				}, map[string][]map[string]interface{}{
+					"明细": []map[string]interface{}{
+						map[string]interface{}{
+							"CODE": "c1",
+							"NAME": "n1",
+							"NUM":  float64(111.22),
+						},
+						map[string]interface{}{
+							"CODE": "c2",
+							"NAME": "n2",
+							"NUM":  float64(111.11),
+						},
+					},
 				}, user); err != nil {
 				t.Error(err)
 			}
@@ -158,12 +322,48 @@ func TestRow2Struct(t *testing.T) {
 			C名称: "name",
 			C数量: 11.2,
 			C描述: "",
+			C其他: []*otherType{
+				&otherType{
+					Name: "n1",
+					Code: "c1",
+					Num:  111.22,
+				}, &otherType{
+					Name: "n2",
+					Code: "c2",
+					Num:  111.11,
+				},
+			},
+			C其他1: []otherType{
+				otherType{
+					Name: "n1",
+					Code: "c1",
+					Num:  111.22,
+				}, otherType{
+					Name: "n2",
+					Code: "c2",
+					Num:  111.11,
+				},
+			},
+			C明细: []otherType{
+				otherType{
+					Name: "n1",
+					Code: "c1",
+					Num:  111.22,
+				}, otherType{
+					Name: "n2",
+					Code: "c2",
+					Num:  111.11,
+				},
+			},
+			innerT: innerT{
+				C嵌入: "aaa",
+			},
 		},
 	},
 	}
 	for _, one := range tt {
 		if !reflect.DeepEqual(one.row1, one.row2) {
-			t.Error(fmt.Errorf("row1:%v <> %v", one.row1, one.row2))
+			t.Error(fmt.Errorf("row not equ:\n%#v\n%#v", one.row1, one.row2))
 		}
 	}
 }
@@ -177,7 +377,7 @@ func mapEqu(row1, row2 map[string]interface{}) error {
 			return fmt.Errorf("key:%s not found at row2", k)
 		}
 		if !reflect.DeepEqual(v, ov) {
-			return fmt.Errorf("key:%s,val: %v <> %v", k, v, ov)
+			return fmt.Errorf("key:%s,val not equ:\n%v\n%v", k, v, ov)
 		}
 	}
 	for k := range row2 {
