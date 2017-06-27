17f16cfeec9333c8f8dbaf2d5281c3a3d2194bf1
linlexing <linlexing@gmail.com>
2017-06-27 09:41:39 +0800

    MarshalJSON 改成非指针，因为有不用指针的地方，且非指针也可以应用指针

--- schema/datatype.go
+++ schema/datatype.go
@@ -40,7 +40,7 @@ const (
 )
 
 //MarshalJSON 实现json的自定义的json序列化，主要是为了兼容前个直接保存字符串值的版本
-func (d *DataType) MarshalJSON() ([]byte, error) {
+func (d DataType) MarshalJSON() ([]byte, error) {
 	return json.Marshal(d.String())
 }
 
