067b8d076945e23c6592c6dd0e228169c175da0a
linlexing <linlexing@gmail.com>
2022-07-29 17:02:40 +0800

    添加varchar类型，支持人大金仓

--- schema/postgres/opentab.go
+++ schema/postgres/opentab.go
@@ -108,7 +108,7 @@ func getTableColumns(db common.DB, schemaName, tableName string) ([]columnType, 
 	columns := []columnType{}
 	strSQL := `select column_name as "DBNAME",
 					(case when is_nullable='YES' then 1 else 0 end) as "DBNULL",
-					(case when data_type in ('text', 'character varying','jsob','jsonb','ARRAY','USER-DEFINED',
+					(case when data_type in ('text', 'varchar','character varying','jsob','jsonb','ARRAY','USER-DEFINED',
 						'uuid','boolean','daterange','int8range','numrange','tsrange')
 						then 'STR'
 						when  data_type in ('smallint','integer','bigint')
