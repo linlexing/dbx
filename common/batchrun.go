996c99a422b17d68194f3cb99db0ee2fe17093b2
Bo0 <425022819@qq.com>
2026-04-22 17:54:10 +0800

    bugfix-common.BatchRun的list如果有的sql需要param有的不需要则会报错

--- common/batchrun.go
+++ common/batchrun.go
@@ -1,8 +1,11 @@
 package common
 
-import "log"
+import (
+	"errors"
+	"log"
+)
 
-//BatchRunAndPrint 批量运行sql并打印
+// BatchRunAndPrint 批量运行sql并打印
 func BatchRunAndPrint(db Execer, list []string, param ...interface{}) error {
 	for _, one := range list {
 		log.Println(one)
@@ -12,10 +15,14 @@ func BatchRunAndPrint(db Execer, list []string, param ...interface{}) error {
 	}
 	return nil
 }
-func BatchRun(db Execer, list []string, param ...interface{}) error {
-	for _, one := range list {
-
-		if _, err := db.Exec(one, param...); err != nil {
+func BatchRun(db Execer, list []string, param ...[]interface{}) error {
+	if len(param) == 0 {
+		param = make([][]interface{}, len(list))
+	} else if len(param) != len(list) {
+		return errors.New("sql与param数量不符")
+	}
+	for i, one := range list {
+		if _, err := db.Exec(one, param[i]...); err != nil {
 			return err
 		}
 	}
